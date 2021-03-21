49e68bd2caa8c8d9653db1285816d69c1ac69287
JSON <avezard.jeason@gmail.com>
2021-03-21 16:24:47 +0100

    init

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="container">
+    <view class="statusbar">
+      <text>My Vue Native App</text>
+    </view>
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: white;
+  flex: 1;
+}
+
+.statusbar {
+  background-color: yellow;
+  height: 20%;
+  margin-top: 7%;
+}
+</style>
---
+++ start_android.sh
@@ -0,0 +1,1 @@
+/home/json/Android/Sdk/emulator/emulator -avd Pixel_3a_API_30_x86
